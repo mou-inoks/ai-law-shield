7e8ac526b687d5985275ee40180cd38cbbde95f0
mou-inoks <mou@inokscapital.com>
2024-11-13 17:55:23 +0100

    in progress

---
+++ dataset/filter_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sklearn.model_selection as train_test_split
+from transformers import BertTokenizer
+
+# Load the dataset
+df = pd.read_csv("generated/contracts_with_level_of_danger.csv")
+
+# Split the dataset for the training 
+train_df, test_df = train_test_split(df, test_size=0.2, test_size=0.2, random_state=42)
+
+#init token BERT 
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+def preprocess_data(dataframe):
+    texts = dataframe['texte_contrat'].tolist()
+    labels = dataframe['niveau_danger'].tolist()
+    
+    # Tokenization et padding
+    encoded_data = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
